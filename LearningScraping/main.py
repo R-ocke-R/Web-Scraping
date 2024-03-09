96c1ef177e0f3d1d8c86bbf850cc8b1de8ac49fe
Manu S <sharamamnu2727@gmail.com>
2024-03-09 20:08:32 +0530

    Successfully Scraped William Ackman's real time net worth

--- LearningScraping/main.py
+++ LearningScraping/main.py
@@ -3,20 +3,22 @@ from bs4 import BeautifulSoup
 
 URL ="https://realpython.github.io/fake-jobs/"
 URLforbes = "https://www.forbes.com/profile/william-ackman/?sh=673a17d0298d"
-page = requests.get(URL)
+page = requests.get(URLforbes)
 
 # print(page.text)
 soup = BeautifulSoup(page.content, "html.parser")
-print(soup.prettify)
+#print(soup.prettify)
 
 # finding information using the tags
-tags = soup.find_all('h2')
-print(tags)
+# tags = soup.find_all('h2')
+# print(tags)
 
 # extracting only the information not the complete tag easy done.
-for tag in tags:
-    print(tag.text)
+# for tag in tags:
+#     print(tag.text)
+# soup.find_all('div', class_='card')
 
 
-    # soup.find_all('div', class_='card')
-
+# now I am going to work towards getting real time value of william ackman from his forbes profile
+user_header = soup.find('div', class_='profile-info__item')
+print(user_header.text)
